cd15d75fa54bc4a7bbcd3250328524d189945c5c
ROVA <107459719+ROVAToken@users.noreply.github.com>
2022-07-12 10:04:48 +0530

    Update ROVA.sol

--- ROVA.sol
+++ ROVA.sol
@@ -1,10 +1,15 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.14;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/utils/SafeERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
 
-contract ROVATOKEN is ERC20, Ownable {
-    constructor(uint256 initialSupply) ERC20("ROVA", "ROVA") {
+contract ROVATOKEN is ERC20, Ownable, ERC20Burnable {
+    constructor(uint256 initialSupply) ERC20("ROVA2", "ROVA2") {
         _mint(msg.sender, initialSupply*(10**uint256(decimals())));
     }
 
